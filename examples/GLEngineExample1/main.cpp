a9e4ca609fa7da09c4e6c47e131ccc3fae0fc899
Asger Nyman Christiansen <asgernyman@gmail.com>
2016-12-18 17:32:35 +0100

    Added example xcode project.

---
+++ examples/GLEngineExample1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLEngineExample1
+//
+//  Created by Asger Nyman Christiansen on 18/12/2016.
+//  Copyright © 2016 Asger Nyman Christiansen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
