3baa95d0566b53d27a0649ce0116935bca5d4b07
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-02-02 21:10:31 +0100

    Using SDL to load texture images.

--- include/GLTexture.h
+++ include/GLTexture.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "GLUtility.h"
+#include <SDL_image.h>
 
 namespace gle
 {
@@ -31,6 +32,34 @@ namespace gle
                          format,
                          GL_UNSIGNED_BYTE,
                          data);
+        }
+        
+        void bind_image(std::string filename, GLenum target)
+        {
+            auto image = loadSurface(filename);
+            unsigned int width = image->w;
+            unsigned int height = image->h;
+            
+            int bytesPerPixel = image->format->BytesPerPixel;
+            auto internal_format = bytesPerPixel == 3 ? GL_RGB : GL_RGBA;
+            auto format = getFormat(image);
+            
+            char *pixels = static_cast<char *>(image->pixels);
+            
+            invert_image(width * bytesPerPixel, height, pixels);
+            
+            glTexImage2D(target,
+                         0,
+                         (GLint) internal_format,
+                         (GLsizei)width,
+                         (GLsizei)height,
+                         0,
+                         format,
+                         GL_UNSIGNED_BYTE,
+                         pixels);
+            
+            SDL_FreeSurface(image);
+            check_gl_error();
         }
         
         /**
@@ -63,6 +92,75 @@ namespace gle
             glActiveTexture(GL_TEXTURE0 + location);
             bind();
         }
+        
+    private:
+        
+        GLenum getFormat(SDL_Surface *image)
+        {
+            assert(image->format->BitsPerPixel == 24 || image->format->BitsPerPixel == 32);
+            
+            GLint Colors = image->format->BytesPerPixel;
+            if (Colors == 4 )
+            {
+                if (image->format->Rmask == 0x000000ff)
+                    return GL_RGBA;
+                else
+                    return GL_BGRA;
+            }
+            else if (Colors == 3)
+            {
+                if (image->format->Rmask == 0x000000ff)
+                    return GL_RGB;
+                else
+                    return GL_BGR;
+            }
+            std::runtime_error("Unknown image format. Only PNG-24 and JPEG is supported.");
+            return NULL;
+        }
+        
+        int invert_image(int width, int height, void *image_pixels)
+        {
+            auto temp_row = std::unique_ptr<char>(new char[width]);
+            if (temp_row.get() == nullptr)
+            {
+                std::runtime_error("Not enough memory for image inversion");
+                return -1;
+            }
+            //if height is odd, don't need to swap middle row
+            int height_div_2 = height / 2;
+            for (int index = 0; index < height_div_2; index++) {
+                //uses string.h
+                memcpy((Uint8 *)temp_row.get(),
+                       (Uint8 *)(image_pixels)+
+                       width * index,
+                       width);
+                
+                memcpy(
+                       (Uint8 *)(image_pixels)+
+                       width * index,
+                       (Uint8 *)(image_pixels)+
+                       width * (height - index - 1),
+                       width);
+                memcpy(
+                       (Uint8 *)(image_pixels)+
+                       width * (height - index - 1),
+                       temp_row.get(),
+                       width);
+            }
+            return 0;
+        }
+        
+        SDL_Surface* loadSurface( std::string path )
+        {
+            //Load image at specified path
+            SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+            if( loadedSurface == NULL )
+            {
+                printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+            }
+            
+            return loadedSurface;
+        }
     };
     
     /**
@@ -78,6 +176,17 @@ namespace gle
         {
             bind();
             bind_image(data, width, height, format, GL_TEXTURE_2D);
+            
+            glTexParameteri(target, GL_TEXTURE_MIN_FILTER, minMagFilter);
+            glTexParameteri(target, GL_TEXTURE_MAG_FILTER, minMagFilter);
+            glTexParameteri(target, GL_TEXTURE_WRAP_S, wrapMode);
+            glTexParameteri(target, GL_TEXTURE_WRAP_T, wrapMode);
+        }
+        
+        GLTexture2D(std::string filename) : GLTexture(GL_TEXTURE_2D)
+        {
+            bind();
+            bind_image(filename, GL_TEXTURE_2D);
             
             glTexParameteri(target, GL_TEXTURE_MIN_FILTER, minMagFilter);
             glTexParameteri(target, GL_TEXTURE_MAG_FILTER, minMagFilter);
