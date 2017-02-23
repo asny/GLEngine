6f11e559206152c9dc25b4cf3759b18dde6ceae4
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-02-23 19:16:56 +0100

    Moved GLFogEffect to its own file.

--- include/GLPostEffect.h
+++ include/GLPostEffect.h
@@ -31,31 +31,4 @@ namespace gle
             GLObject::draw_full_screen_quad(shader);
         }
     };
-    
-    class GLFogEffect : public GLPostEffect
-    {
-        std::shared_ptr<GLTexture> noise_texture;
-        std::shared_ptr<mesh::Attribute<mesh::VertexID, glm::vec2>> uv_coordinates;
-        std::shared_ptr<float> time;
-    public:
-        GLFogEffect(std::shared_ptr<GLTexture> _noise_texture, std::shared_ptr<float> _time)
-            : GLPostEffect("../GLEngine/shaders/fog_effect.vert",  "../GLEngine/shaders/fog_effect.frag"), time(_time), noise_texture(_noise_texture)
-        {
-            
-        }
-        
-        void apply(const GLRenderTarget& source_render_target, float z_near, float z_far)
-        {
-            source_render_target.bind_depth_texture_for_reading(1);
-            noise_texture->use(2);
-            
-            GLUniform::use(shader, "depthMap", 1);
-            GLUniform::use(shader, "noiseTexture", 2);
-            GLUniform::use(shader, "time", *time);
-            GLUniform::use(shader, "zNear", z_near);
-            GLUniform::use(shader, "zFar", z_far);
-            
-            GLPostEffect::apply(source_render_target, z_near, z_far);
-        }
-    };
 }
