97792464e4f01091bc528870cc163b7a758e4bda
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-02-23 18:41:37 +0100

    Moved draw full screen quad functionality to GLObject.

--- include/GLPostEffect.h
+++ include/GLPostEffect.h
@@ -28,48 +28,7 @@ namespace gle
             
             GLUniform::use(shader, "colorMap", 0);
             
-            // Bind vertex array and draw
-            shader->use();
-            draw_full_screen_quad();
-        }
-    private:
-        
-        void draw_full_screen_quad()
-        {
-            static GLuint array_id = NULL_LOCATION;
-            if(array_id == NULL_LOCATION)
-            {
-                // Generate and bind array
-                glGenVertexArrays(1, &array_id);
-                glBindVertexArray(array_id);
-                
-                // Create mesh
-                auto mesh = std::make_shared<mesh::Mesh>();
-                mesh::VertexID* v1 = mesh->create_vertex(glm::vec3(-3., -1., 0.));
-                mesh::VertexID* v2 = mesh->create_vertex(glm::vec3(3., -1., 0.));
-                mesh::VertexID* v3 = mesh->create_vertex(glm::vec3(0., 2., 0.));
-                mesh->create_face(v1, v3, v2);
-                
-                auto uv_coordinates = std::make_shared<mesh::Attribute<mesh::VertexID, glm::vec2>>();
-                uv_coordinates->at(v1) = glm::vec2(-1., 0.);
-                uv_coordinates->at(v2) = glm::vec2(2., 0.);
-                uv_coordinates->at(v3) = glm::vec2(0.5, 1.5);
-                
-                // Create attribute and send data.
-                auto position = shader->create_attribute("position", mesh->position());
-                position->add_data_at(*v1);
-                position->add_data_at(*v2);
-                position->add_data_at(*v3);
-                position->send_data();
-                
-                auto uv = shader->create_attribute("uv_coordinates", uv_coordinates);
-                uv->add_data_at(*v1);
-                uv->add_data_at(*v2);
-                uv->add_data_at(*v3);
-                uv->send_data();
-            }
-            glBindVertexArray(array_id);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            GLObject::draw_full_screen_quad(shader);
         }
     };
     
