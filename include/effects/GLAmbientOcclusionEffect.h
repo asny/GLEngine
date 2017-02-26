7578ed0ad16e3e36e4acd7192c5c43ffd35f5fd2
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-02-26 15:38:08 +0100

    Added sample kernel and noise texture

--- include/effects/GLAmbientOcclusionEffect.h
+++ include/effects/GLAmbientOcclusionEffect.h
@@ -11,20 +11,63 @@ namespace gle
 {
     class GLAmbientOcclusionEffect : public GLPostEffect
     {
+        std::unique_ptr<GLTexture2D> sample_texture, noise_texture;
     public:
         
         GLAmbientOcclusionEffect()
         : GLPostEffect("../GLEngine/shaders/effect.vert",  "../GLEngine/shaders/ssao_effect.frag")
         {
-            
+            create_sample_kernel();
+            create_noise_texture();
         }
         
         void apply(const GLRenderTarget& source_render_target1, const GLRenderTarget& source_render_target2, float z_near, float z_far)
         {
+            sample_texture->use(4);
+            noise_texture->use(5);
             
+            GLUniform::use(shader, "sampleTexture", 4);
+            GLUniform::use(shader, "noiseTexture", 5);
             
             GLPostEffect::apply(source_render_target1, source_render_target2, z_near, z_far);
         }
+        
+    private:
+        
+        void create_sample_kernel()
+        {
+            const int kernel_size = 4;
+            auto kernel = std::vector<float>(kernel_size * kernel_size * 3);
+            for (int i = 0; i < kernel_size * kernel_size; ++i)
+            {
+                auto sample = glm::normalize(glm::vec3(random(-1.0f, 1.0f), random(-1.0f, 1.0f), random(0.0f, 1.0f)));
+                sample *= random(0.0f, 1.0f);
+                float scale = float(i) / float(kernel_size);
+                float t = scale * scale;
+                sample *= (1. - t) * 0.1 + t;
+                
+                kernel[i*3] = sample.x;
+                kernel[i*3+1] = sample.y;
+                kernel[i*3+2] = sample.z;
+            }
+            sample_texture = std::unique_ptr<GLTexture2D>(new GLTexture2D(&kernel[0], kernel_size, kernel_size));
+        }
+        
+        void create_noise_texture()
+        {
+            const int noise_size = 4;
+            auto noise = std::vector<float>(noise_size * noise_size * 3);
+            for (int i = 0; i < noise_size * noise_size; ++i)
+            {
+                auto n = normalize(glm::vec3(
+                                random(-1.0f, 1.0f),
+                                random(-1.0f, 1.0f),
+                                0.0f));
+                noise[i*3] = n.x;
+                noise[i*3+1] = n.y;
+                noise[i*3+2] = n.z;
+            }
+            noise_texture = std::unique_ptr<GLTexture2D>(new GLTexture2D(&noise[0], noise_size, noise_size));
         }
     };
 }
--- shaders/ssao_effect.frag
+++ shaders/ssao_effect.frag
@@ -4,6 +4,10 @@ uniform sampler2D colorMap;
 uniform sampler2D normalMap;
 uniform sampler2D depthMap;
 
+uniform sampler2D sampleTexture;
+uniform sampler2D noiseTexture;
+uniform vec2 noiseScale;
+
 in vec2 uv;
 
 layout (location = 0) out vec4 color;
@@ -14,5 +18,12 @@ void main()
    	vec3 normal = normalize(texture(normalMap, uv).xyz);
     float depth = texture(depthMap, uv).r;
     
-    color = vec4(light_color, 1.);
+    vec3 rvec = texture(noiseTexture, uv).xyz;
+    vec3 tangent = normalize(rvec - normal * dot(rvec, normal));
+    vec3 bitangent = cross(normal, tangent);
+    mat3 tbn = mat3(tangent, bitangent, normal);
+    
+    
+    vec3 kernel_sample = texture(sampleTexture, uv).xyz;
+    color = vec4(normal, 1.);
 }
