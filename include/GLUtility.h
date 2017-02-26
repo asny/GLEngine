998644ae3a431f45b96823ae4be7c1cfed196489
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-02-26 13:36:47 +0100

    Added time function to GLUtility.

--- include/GLUtility.h
+++ include/GLUtility.h
@@ -21,26 +21,36 @@
 #include "gtc/matrix_transform.hpp"
 #include "gtc/matrix_inverse.hpp"
 
-const static unsigned int NULL_LOCATION = -1;
-
-inline void _check_gl_error(const char *file, int line)
-{
-    GLenum err (glGetError());
+namespace gle {
     
-    while(err!=GL_NO_ERROR) {
-        std::string error;
+    const static unsigned int NULL_LOCATION = -1;
+    
+    inline void _check_gl_error(const char *file, int line)
+    {
+        GLenum err (glGetError());
         
-        switch(err) {
-            case GL_INVALID_OPERATION:      error="INVALID_OPERATION";      break;
-            case GL_INVALID_ENUM:           error="INVALID_ENUM";           break;
-            case GL_INVALID_VALUE:          error="INVALID_VALUE";          break;
-            case GL_OUT_OF_MEMORY:          error="OUT_OF_MEMORY";          break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION:  error="INVALID_FRAMEBUFFER_OPERATION";  break;
+        while(err!=GL_NO_ERROR) {
+            std::string error;
+            
+            switch(err) {
+                case GL_INVALID_OPERATION:      error="INVALID_OPERATION";      break;
+                case GL_INVALID_ENUM:           error="INVALID_ENUM";           break;
+                case GL_INVALID_VALUE:          error="INVALID_VALUE";          break;
+                case GL_OUT_OF_MEMORY:          error="OUT_OF_MEMORY";          break;
+                case GL_INVALID_FRAMEBUFFER_OPERATION:  error="INVALID_FRAMEBUFFER_OPERATION";  break;
+            }
+            
+            std::cerr << "GL_" << error.c_str() <<" - "<<file<<":"<<line<<std::endl;
+            err=glGetError();
         }
-        
-        std::cerr << "GL_" << error.c_str() <<" - "<<file<<":"<<line<<std::endl;
-        err=glGetError();
+    }
+    
+#define check_gl_error() _check_gl_error(__FILE__,__LINE__)
+    
+    static float time()
+    {
+        static auto start_time = std::chrono::high_resolution_clock::now();
+        auto current_time = std::chrono::high_resolution_clock::now();
+        return 0.001f * std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count();
     }
 }
-
-#define check_gl_error() _check_gl_error(__FILE__,__LINE__)
