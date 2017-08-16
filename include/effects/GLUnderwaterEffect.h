52c1d719690fdc5d3b47dca910ba5ed0c89f3c1e
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-08-16 11:40:59 +0200

    Underwater effect.

---
+++ include/effects/GLUnderwaterEffect.h
@@ -0,0 +1,45 @@
+//
+//  Created by Asger Nyman Christiansen on 22/02/2017.
+//  Copyright © 2017 Asger Nyman Christiansen. All rights reserved.
+//
+
+#pragma once
+
+#include "GLPostEffect.h"
+
+namespace gle
+{
+    class GLUnderwaterEffect : public GLPostEffect
+    {
+    public:
+        glm::vec3 color = glm::vec3(0.5, 0.5, 0.8);
+        float density = 0.2;
+        float no_fog_height = 3;
+        float animation = 0.1;
+        
+        GLUnderwaterEffect()
+        : GLPostEffect("../GLEngine/shaders/effect.vert",  "../GLEngine/shaders/underwater_effect.frag")
+        {
+            
+        }
+        
+        void apply(const DrawPassInput& input) const
+        {
+            auto shader = get_shader();
+            GLState::depth_write(false);
+            GLState::depth_test(false);
+            
+            input.position_texture->use(0);
+            GLUniform::use(shader, "positionMap", 0);
+            
+            GLUniform::use(shader, "waterColor", color);
+            GLUniform::use(shader, "fogDensity", density);
+            GLUniform::use(shader, "noFogHeight", no_fog_height);
+            GLUniform::use(shader, "animation", animation);
+            GLUniform::use(shader, "time", time());
+            GLUniform::use(shader, "eyePosition", input.camera_position);
+            
+            GLObject::draw_full_screen_quad(shader);
+        }
+    };
+}
